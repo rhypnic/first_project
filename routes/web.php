32ba9408bc932b8c8683e4d83ef76117b7d0daf3
rhypnic <alfinindrawan54@gmail.com>
2021-01-27 20:46:20 +0700

    tugas_laravel_templete

--- routes/web.php
+++ routes/web.php
@@ -11,12 +11,22 @@
 |
 */
 
-Route::get('/home', function () {
+Route::get('/laravel', function () {
     return view('welcome');
 });
 Route::get('/test', function () {
     return "ok";
 });
-Route::get('/', 'HomeController@form' );
+Route::get('/home', 'HomeController@form' );
 Route::get('/register', 'AuthController@regis');
 Route::post('/welcome', 'AuthController@welcome');
+/* php -S localhost:8000 -t public harus pake ini jgn php artisan serve */
+Route::get('/blank',  function () {
+    return view('tugas_laravel_2.blank');
+});
+Route::get('/',  function () {
+    return view('tugas_laravel_2.index');
+});
+Route::get('/data-tables',  function () {
+    return view('tugas_laravel_2.datatables');
+});
